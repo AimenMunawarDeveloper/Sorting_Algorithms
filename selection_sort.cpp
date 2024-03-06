0543bdb3508e032200be7d87ce06eae174c09fd9
Aimen Munawar <aimenmunawarofficial@gmail.com>
2024-03-06 20:57:20 +0500

    selection_sort

---
+++ selection_sort.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+// Function to perform selection sort on array A of size n
+void selectionSort(int A[], int n) {
+    // Traverse through all array elements
+    for (int i = 0; i < n - 1; i++) {
+        // Assume the current index is the smallest
+        int smallest = i;
+
+        // Find the index of the smallest element in the unsorted part
+        for (int j = i + 1; j < n; j++) {
+            if (A[j] < A[smallest]) {
+                smallest = j;
+            }
+        }
+
+        // Swap the found smallest element with the current element
+        int temp = A[i];
+        A[i] = A[smallest];
+        A[smallest] = temp;
+    }
+}
+
+int main() {
+    int array[10] = {4, 77, 24, 6, 2, 8, 3, 6, 4, 4};
+
+    cout << "Array before Sorting:\n";
+    for (int i = 0; i < 10; i++) {
+        cout << array[i] << " ";
+    }
+
+    // Call the selectionSort function to sort the array
+    selectionSort(array, 10);
+
+    cout << "\nArray After Sorting:\n";
+    for (int i = 0; i < 10; i++) {
+        cout << array[i] << " ";
+    }
+
+    return 0;
+}
